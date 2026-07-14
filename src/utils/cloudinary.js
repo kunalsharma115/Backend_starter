741d7896d5f41974bdfdf8435fd0af384696e018
Kunal Sharma <kunalsharma115@gmail.com>
2026-07-14 12:06:29 +0000

    File Upload Using Clounary

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,29 @@
+import {v2 as cloudinary} from 'cloudinary'
+import fs from 'fs' 
+
+cloudinary.config({
+    cloud_name:  process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:  process.env.CLOUDINARY_API_KEY,
+    api_secrets:  process.env.CLOUDINARY_API_SECRET,
+})
+
+const uploadONCloudnary = async(localFilePath) =>
+{
+    try {
+        if(!localFilePath) return null
+
+        const response = await cloudinary.uploader.upload(localFilePath, {
+            resource_type: "auto"
+        })
+        //file has been uploaded succefully
+
+        console.log("file is uploaded on cloudinary", response.url);
+        return response;
+    } catch (error) {
+        fs.unlinkSync(localFilePath) // removes localy saved temp file as upload option got failed
+
+    }
+
+}
+
+export {uploadONCloudnary}
