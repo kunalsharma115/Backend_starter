aaff494da3cf2414cdf81d9fb50759cbb0f126f5
Kunal Sharma <kunalsharma3411@gmail.com>
2026-09-11 07:45:02 +0000

    mongo pipeline

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -354,6 +354,89 @@ const updateCoverImage = asyncHandler(async(req ,res)=>
    )
 })
 
+const getUserChannelProfile = asyncHandler(async(req , res )=>{
+
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400)
+    }
+
+    const channel = await User.aggregate([
+            {
+                $match:{
+                    username : username?.toLowerCase()
+                },
+            },
+
+            {
+                $lookup:{
+                    from: "subscriptions",
+                    localField:"_id",
+                    foreignField:"channel",
+                    as:"subscribes",
+                }
+            },
+
+            { 
+                $lookup:
+                {
+                    from:"subscriptions",
+                    localField:"_id",
+                    foreignField:"subscribers",
+                    as :"subscribedTo",
+                }
+            },
+
+            {
+                $addFields:
+                {
+                    subscribersCount:
+                    {
+                        $size : "$subscribers"
+                    },
+                    channelsSubscribedToCount:
+                    {
+                        $size : "$subscribedTo"
+                    },
+                    isSubscribed: 
+                    {
+                        $cond:{
+                            if : {$in :[req.user?._id, "&subscribers.subscriber"]},
+                            then:true,
+                            else:false,
+                        }
+                    }
+                }
+                
+            },
+
+            {
+                $project:
+                {
+                    fullName:1,
+                    username:1,
+                    isSubscribed:1,
+                    subscribersCount:1,
+                    channelsSubscribedToCount:1,
+                    avatar:1,
+                    coverImage:1,
+                    email:1,
+
+                }
+            }
+
+         
+        
+    ])
+
+    if(!channel?.length)
+    {
+        throw new ApiError(404, "Channel doesnt exists")
+    }
+}
+)
+
 export { 
     registerUser ,
     loginUser,
@@ -362,5 +445,7 @@ export {
     changeCurrentPassword,
     getCurrentUser,
     updateAccountDetails,
-    updateUserAvatar
+    updateUserAvatar,
+    updateCoverImage,
+    getUserChannelProfile
 }
